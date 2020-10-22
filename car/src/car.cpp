02bd524c1ebdb37f33122d3f215773af4a8e70ea
Nikhil Singhal <niks@mit.edu>
2020-10-22 06:42:56 -0400

    Add minimal physics sim

--- car/src/car.cpp
+++ car/src/car.cpp
@@ -14,35 +14,65 @@
 class CarNode {
   ros::NodeHandle nh;
 
-  ros::Subscriber controlSub;
-  ros::Publisher posePub;
-  ros::Timer timer;
+  ros::Subscriber controlSub_;
+  ros::Publisher posePub_;
+  ros::Timer timer_;
 
-  tf2_ros::TransformBroadcaster br;
+  tf2_ros::TransformBroadcaster br_;
 
-  double x;
-  double y;
-  double theta;
-  double v;
+  double hz_ = 30;
 
-  double L; // length between wheels/wheelbase
-  double lfw; // length ahead of rear wheel to plan from
-  double max_accel;
+  double x_;
+  double y_;
+  double theta_;
+  double v_;
+  double delta_; // steering angle
 
-  double desired_steering_angle; // delta, rad
-  double desired_speed; // m/s
+  double L_ = 0.7; // length between wheels/wheelbase
+  double lfw_ = 0.15; // length ahead of rear wheel to plan from
+  double max_accel_ = 1; // m/s^2
+  double max_vel_ = 8; // m/s
+  double width_ = 0.3;
+  double length_ = 0.8;
+  double height_ = 0.15; // doesn't matter
+
+  double desired_steering_angle_ = .1; // rad
+  double desired_speed_ = 1; // m/s
+
+  ros::Time last_step_;
 
 public:
-  CarNode(): x(0), y(0), theta(0) {
-    controlSub = nh.subscribe("car_control", 1, &CarNode::onControl, this);
-    posePub = nh.advertise<geometry_msgs::PoseStamped>("car_pose", 1, this);
-    timer = nh.createTimer(ros::Duration(1/30.0), &CarNode::simLoop, this);
+  CarNode(): x_(0), y_(0), theta_(0), v_(0), delta_(0) {
+    controlSub_ = nh.subscribe("car_control", 1, &CarNode::onControl, this);
+    posePub_ = nh.advertise<geometry_msgs::PoseStamped>("car_pose", 1, this);
+    last_step_ = ros::Time::now();
+    timer_ = nh.createTimer(ros::Duration(1/hz_), &CarNode::simLoop, this);
   }
 
-  void onControl(const ackermann_msgs::AckermannDrive& control) {}
+  void onControl(const ackermann_msgs::AckermannDrive& control) {
+    desired_steering_angle_ = control.steering_angle;
+    desired_speed_ = control.speed;
+  }
 
   void simLoop(const ros::TimerEvent& t) {
     std::cout << "in sim loop\n";
+
+    ros::Time time = ros::Time::now();
+    double dt = (time - last_step_).toSec();
+    last_step_ = time;
+
+    x_ += cos(theta_)*v_*dt;
+    y_ += sin(theta_)*v_*dt;
+    delta_ = desired_steering_angle_; // for now it updates immediately
+    theta_ += v_*tan(delta_)/L_*dt;
+
+    if(desired_speed_ < v_) {
+      v_ = std::max(-max_vel_, std::max(v_ - max_accel_*dt, desired_speed_));
+    } else {
+      v_ = std::min(max_vel_, std::min(v_ + max_accel_*dt, desired_speed_));
+    }
+
+    std::cout << "x: " << x_ << " y: " << y_ << std::endl;
   }
 };
 
